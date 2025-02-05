75b70af9cfd05815e80b67ab835f18902eda4f93
Dj3ni <fernandezgarcia.jenny@gmail.com>
2025-02-05 19:38:02 +0100

    update script DB + publish ok

--- DB/Scripts/Script.PostDeployment.sql
+++ DB/Scripts/Script.PostDeployment.sql
@@ -9,9 +9,8 @@ Modèle de script de post-déploiement
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
-
---Ajout des rôles nécessaires à l'appli : User et Admin
-Insert Into [Role] Values ("USER"),("ADMIN");
+-- Ajout des rôles nécessaire à l'application : User et Admin
+INSERT INTO [Role] VALUES ('User'),('Admin');
 
 -- Déclaration de la table variable pour stocker les IDs insérés
 DECLARE @InsertedUserIds TABLE (UserId UNIQUEIDENTIFIER);
@@ -23,6 +22,7 @@ EXEC SP_User_Insert @first_name = N'John', @last_name = N'Doe', @email = N'john.
 
 DECLARE @User1Id UNIQUEIDENTIFIER;
 SELECT @User1Id = UserId FROM @InsertedUserIds;
+EXEC SP_User_ChangeRole @user_id=@User1Id, @role = N'Admin'
 DELETE FROM @InsertedUserIds;
 
 INSERT INTO @InsertedUserIds (UserId)
